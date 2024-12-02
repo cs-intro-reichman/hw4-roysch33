04ef3a290d937d2587c32f69354e4e928ca01a26
roy <roysch33@gmail.com>
2024-12-02 22:04:55 +0200

    finished MyString

--- MyString.java
+++ MyString.java
@@ -1,3 +1,5 @@
+
+
 public class MyString {
     public static void main(String[] args) {
         System.out.println("Testing lowercase:");
@@ -14,17 +16,45 @@ public class MyString {
         System.out.println(contains("personality", "son")); // true
         System.out.println(contains("personality", "dad")); // false
         System.out.println(contains("resignation", "sign")); // true
+
     }
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String resultString = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            
+            // Check if the character is an uppercase letter
+            if (ch >= 'A' && ch <= 'Z') {
+                // Convert to lowercase by adding the difference between 'a' and 'A'
+                resultString += ((char) (ch + ('a' - 'A')));
+            } else {
+                // If not uppercase, append the character as is
+                resultString += (ch);
+            }
+        }
+        return resultString;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+        String str2Substring = str2; 
+        String firstString = str1;
+        for (int i = 0; i < str2.length(); i++){
+            for (int j = 0; j < firstString.length(); j++){
+                if (firstString.charAt(j) == ' ' ){
+                    break;
+                }
+                if (str2.charAt(i) == firstString.charAt(j)){
+                    // deleting the first letter in the string and breaking the for loop:
+                    str2Substring = str2Substring.substring(1);
+                    break;
+                }
+            
+            }
+        }
+        return (str2Substring.isEmpty());
     }
 }
